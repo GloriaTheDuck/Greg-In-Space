293364d9e87af67c8e886c8ebe1a9603a62a1844
Lucy Wang <lucywang@Lucys-MacBook-Pro.local>
2020-10-12 16:23:34 -0500

    Updated levels.js

--- levels.js
+++ levels.js
@@ -10,7 +10,35 @@ var x = {
     level1:{
         music: "Sound/lab_gameplay_music.mp3",
         json : "Tilemaps/Lvl1.json"
-    }
+    },
+    level2:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl2.json"
+    },
+    level3:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl3.json"
+    },
+    level4:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl4.json"
+    },
+    level5:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl5.json"
+    },
+    level6:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl6.json"
+    },
+    level7:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl7.json"
+    },
+    level8:{
+        music: "Sound/lab_gameplay_music.mp3",
+        json : "Tilemaps/Lvl8.json"
+    },
 };
 
 export default x;
